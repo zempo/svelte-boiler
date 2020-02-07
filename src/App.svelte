06869a546549171dc9cad2092717089f2c960504
zempo <zelenkosolomon@gmail.com>
2020-02-07 11:34:36 -0800

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+  export let players;
+  let currentPlayer = players[0];
+
+  const callName = () => {
+    console.log(players);
+  };
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: rgb(255, 62, 0);
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1 on:click={callName}>Hello {currentPlayer.name}!</h1>
+  <h3 class="points">{currentPlayer.score}</h3>
+  <h2>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </h2>
+</main>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+import App from "./App.svelte";
+
+const app = new App({
+  target: document.body,
+  props: {
+    players: [
+      {
+        name: "Solomon",
+        score: 100
+      },
+      {
+        name: "Tia",
+        score: 200
+      },
+      {
+        name: "Janet",
+        score: 150
+      }
+    ]
+  }
+});
+
+export default app;
